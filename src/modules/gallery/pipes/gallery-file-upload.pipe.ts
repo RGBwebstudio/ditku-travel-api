7edcf50542af2aff484cc2d6cfc2676eafde1fc2
stnhnd <minimal.wbd@gmail.com>
2026-01-12 20:33:29 +0200

    add ability to upload massive images

--- src/modules/gallery/pipes/gallery-file-upload.pipe.ts
+++ src/modules/gallery/pipes/gallery-file-upload.pipe.ts
@@ -1,31 +1,35 @@
 import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common'
 
-import { UploadedFile } from 'src/common/types/uploaded-file.types'
-
 @Injectable()
 export class GalleryFileUploadPipe implements PipeTransform {
   private readonly limit = 15 * 1024 * 1024 // 15MB
   private readonly acceptableFormats = ['image/jpeg', 'image/png', 'image/webp', 'image/avif']
 
-  transform(value: UploadedFile | UploadedFile[]) {
+  transform(value: Express.Multer.File | Express.Multer.File[]) {
     if (!value) {
       throw new BadRequestException('INVALID_FILE')
     }
 
-    const file = Array.isArray(value) ? value[0] : value
+    const files = Array.isArray(value) ? value : [value]
 
-    if (!file || !file.size) {
+    if (files.length === 0) {
       throw new BadRequestException('INVALID_FILE')
     }
 
-    if (file.size > this.limit) {
-      throw new BadRequestException('File size exceeds the limit of 15MB LIMIT_OF_SIZE')
+    for (const file of files) {
+      if (!file || !file.size) {
+        throw new BadRequestException('INVALID_FILE')
+      }
+
+      if (file.size > this.limit) {
+        throw new BadRequestException(`File ${file.originalname} exceeds the limit of 15MB LIMIT_OF_SIZE`)
+      }
+
+      if (!this.acceptableFormats.includes(file.mimetype)) {
+        throw new BadRequestException(`File ${file.originalname} has unsupported format`)
+      }
     }
 
-    if (!this.acceptableFormats.includes(file.mimetype)) {
-      throw new BadRequestException('UNSUPPORTED_FILE_FORMAT')
-    }
-
-    return file
+    return value
   }
 }
